19b9f3bb880b707adb680bd92d89f36e99ebdf73
Ivan Iliev <73687077+ivan88iliev@users.noreply.github.com>
2021-03-24 03:43:08 +0200

    Add files via upload

---
+++ RegularExpressionsExercise01Furniture.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Regular_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // RegularExpressionsExercise01Furniture
+            string text = Console.ReadLine();
+
+            string pattern = @">>([A-z]+)<<(\d+\.?\d*)!(\d+)";
+            List<string> furnitureList = new List<string>();
+            double totalPrice = 0.00;
+
+            while (text != "Purchase")
+            {
+                Regex shopPattern = new Regex(pattern);
+                Match match = shopPattern.Match(text);
+                string furnitureName = match.Groups[1].ToString();
+                if (match.Success)
+                {
+                    furnitureList.Add(furnitureName);
+                    totalPrice += double.Parse(match.Groups[2].ToString()) * int.Parse(match.Groups[3].ToString());
+
+                }
+
+                text = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (var item in furnitureList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total money spend: {totalPrice:F2}");
+            //
+        }
+    }
+}
